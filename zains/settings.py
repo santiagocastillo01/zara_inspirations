da455a5e7fce69edf76b5b2b32485c87fb638ce2
santiagocastillo01 <castillosantiago441@gmail.com>
2024-08-06 21:08:28 -0300

    local

--- zains/settings.py
+++ zains/settings.py
@@ -1,14 +1,19 @@
 import os
 from pathlib import Path
 
+# Directorio base del proyecto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Clave secreta para el proyecto Django
 SECRET_KEY = 'django-insecure-^hm9ar)k55qj3^d@_^*@@u-(*u43qbl%&yh4z^l6m9wtgf_6v3'
 
-DEBUG = False
+# Depuración activada para el entorno local
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+# Hosts permitidos para aceptar solicitudes
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.vercel.app', '.now.sh']
 
+# Aplicaciones instaladas en el proyecto
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -19,6 +24,7 @@ INSTALLED_APPS = [
     'zar',
 ]
 
+# Middleware utilizado por el proyecto
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -29,12 +35,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Configuración del enrutador principal
 ROOT_URLCONF = 'zains.urls'
 
+# Configuración de plantillas
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'zar/templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # Directorio de plantillas ajustado
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -47,8 +55,10 @@ TEMPLATES = [
     },
 ]
 
+# Aplicación WSGI
 WSGI_APPLICATION = 'zains.wsgi.application'
 
+# Configuración de la base de datos
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -56,6 +66,7 @@ DATABASES = {
     }
 }
 
+# Validadores de contraseñas
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -71,23 +82,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Configuración de idioma y zona horaria
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
+# Configuración de archivos estáticos
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "zar/static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+# Configuración de archivos multimedia
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Configuración de autenticación
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'perfil'
 LOGOUT_REDIRECT_URL = 'login'
+
